f26622e4bb2485a28b986fc17bd0f85975ac24ee
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2019-03-29 11:49:05 +0000

    New Content Package format Rewrite modules to support new content package format

---
+++ Solutions/Endjin.AzureWeekly.Wyam/GetAllContentPacksModule.cs
@@ -0,0 +1,32 @@
+﻿namespace Endjin.AzureWeekly.WyamModules
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using Newtonsoft.Json;
+    using Wyam.Common.Documents;
+    using Wyam.Common.Execution;
+    using Wyam.Common.Modules;
+
+    public class GetAllContentPacksModule : IModule
+    {
+        private string contentPackageDirectory;
+
+        public GetAllContentPacksModule(string contentPackageDirectory)
+        {
+            this.contentPackageDirectory = contentPackageDirectory;
+        }
+
+        public IEnumerable<IDocument> Execute(IReadOnlyList<IDocument> inputs, IExecutionContext context)
+        {
+            var contentPackages = new List<ContentPackage>();
+
+            foreach (var filePath in Directory.EnumerateFiles(this.contentPackageDirectory))
+            {
+                yield return context.GetDocument(new List<KeyValuePair<string, object>>
+                {
+                    new KeyValuePair<string, object>("ContentPackage", JsonConvert.DeserializeObject<ContentPackage>(File.ReadAllText(filePath))),
+                });
+            }
+        }
+    }
+}
---
+++ Solutions/Endjin.AzureWeekly.Wyam/GetContentPackagesForFrontPage.cs
@@ -0,0 +1,40 @@
+﻿namespace Endjin.AzureWeekly.WyamModules
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Newtonsoft.Json;
+    using Wyam.Common.Documents;
+    using Wyam.Common.Execution;
+    using Wyam.Common.Modules;
+
+    public class GetContentPackagesForFrontPage : IModule
+    {
+        private string contentPackageDirectory;
+
+        public GetContentPackagesForFrontPage(string contentPackageDirectory)
+        {
+            this.contentPackageDirectory = contentPackageDirectory;
+        }
+
+        public IEnumerable<IDocument> Execute(IReadOnlyList<IDocument> inputs, IExecutionContext context)
+        {
+            var contentPackages = new List<ContentPackage>();
+
+            foreach (var filePath in Directory.EnumerateFiles(this.contentPackageDirectory))
+            {
+                contentPackages.Add(JsonConvert.DeserializeObject<ContentPackage>(File.ReadAllText(filePath)));
+            }
+
+            foreach (var contentPackage in contentPackages.OrderByDescending(x => x.Issue).Take(4))
+            {
+                var metadata = new List<KeyValuePair<string, object>>
+                {
+                    new KeyValuePair<string, object>("ContentPackage", contentPackage),
+                };
+
+                yield return context.GetDocument(metadata);
+            }
+        }
+    }
+}
